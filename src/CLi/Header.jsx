e4973937e333918326207e82360ca7c8f4c4c873
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-01-20 21:52:46 +0530

    Remove toggle button

--- src/CLi/Header.jsx
+++ src/CLi/Header.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { toggleDeactive } from "../store/toggleSlice";
-
 const Header = () => {
-  const dispatch = useDispatch();
 
-  const handleToggle = () => {
-    dispatch(toggleDeactive());
-  };
 
   return (
-    <div className="bg-blue-500 text-white flex justify-between p-1">
-      <h3 className="ml-4">CP03: /PORTFOLIO/ command-line</h3>
-      <p className="cursor-pointer px-2 hover:bg-red-600" onClick={handleToggle}>
+    <div className="bg-gray-500 text-white flex justify-between p-1">
+      <h3 className="">CP03: /PORTFOLIO/ command-line</h3>
+      {/* <p className="cursor-pointer px-2 hover:bg-red-600" onClick={handleToggle}>
         X
-      </p>
+      </p> */}
     </div>
   );
 };
